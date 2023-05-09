eab49fcb08a4c68bcc08375ff77124509d2cd5ca
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-05-09 09:07:43 +0530

    Add :  Generic Basic Problem amd concept

---
+++ GenericProb/GenericClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProb
+{
+    public  class GenericClass<T>
+    {
+        //variable of type T
+        public T data;
+        //Generic constructor...............
+        public GenericClass(T data)
+        {
+            this.data = data;
+            Console.WriteLine("Data :"+this.data);
+
+
+        }
+        //Generic Method
+        public void GenericMethod(T data)
+        {
+            Console.WriteLine("Data method :"+data);
+        }
+        public void GenericMethodDemo<T>(T data)
+        {
+            Console.WriteLine("Data methodTwo :" + data);
+        }
+
+
+    }
+}
---
+++ GenericProb/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProb
+{
+    //Generic means General From and do not have return type
+    // it means not specific or particular data type
+    // "<>" this angle bracate  with type name
+    //TypeName<T> T is your Type Parameter
+    //Generic allow us to create single class or method that can be use diff data Type
+    //Advantage : we can reuse our code
+
+    public  class Program
+    {
+        public static void Main(string[] args)
+        {
+            GenericClass<string> genericClass = new GenericClass<string>("Krishna");
+            GenericClass<int> generic = new GenericClass<int>(41);
+            GenericClass<double> genericdouble = new GenericClass<double>(2.345);
+
+            genericClass.GenericMethod("Raj");
+            generic.GenericMethod(45);
+            genericdouble.GenericMethod(3.12);
+            genericClass.GenericMethodDemo<int>(78);
+            genericClass.GenericMethodDemo<double>(5.56);
+            Console.ReadLine();
+
+
+        }
+    }
+}
